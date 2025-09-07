d357d51f0c49acdd5c19a3d9f1cebd0048a03565
David <david.manukyan.code@gmail.com>
2025-09-08 02:13:44 +0400

    update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    host: true,
+    allowedHosts: ['crm-google.onrender.com'] // optional, for preview only
+  }
+})
